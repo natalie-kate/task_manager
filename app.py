2619d9857a0a583ab00aa663033e5d4baff5ccad
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-08-14 20:45:18 +0000

    profile fuction added

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -58,20 +59,30 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-             existing_user["password"], request.form.get("password")):
+              existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
                 # invalid password match
-                flash("Incorrect username and/or Password")
+                flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
             # username doesn't exist
-            flash("Incorrect username and/or password")
+            flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
